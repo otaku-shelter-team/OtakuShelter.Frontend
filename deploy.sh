fdd1d591315b0fd0cc386b61ea73fac5fb6bca82
Sergey <sergeyshaykhullin@gmail.com>
2019-02-21 13:38:00 +0300

    1.0.0 Dockerize

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
+
+echo docker pull otakushelter/frontend:1.0.$TRAVIS_BUILD_NUMBER
+echo docker rm \$\(docker stop \$\(docker ps -a -q --filter ancestor=otakushelter/frontend:1.0.$PREVIOUS_VERSION --format="{{.ID}}"\)\)
+echo docker rmi otakushelter/frontend:1.0.$PREVIOUS_VERSION -f 
+echo docker run -d -p 127.0.0.1:4004:80 otakushelter/frontend:1.0.$TRAVIS_BUILD_NUMBER
