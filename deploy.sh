48bc0d3dd4dd969f4736416bb10c4792408cf3bb
Sergey <sergeyshaykhullin@gmail.com>
2019-02-25 20:21:36 +0300

    1.0.0 New travis config

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "cd /root/OtakuShelter.Infrastructure/src && \
-ansible-playbook deploy.yml \
--e \"\
-otakushelter_hosts=frontends \
-otakushelter_container=otakushelter_frontend \
-otakushelter_image=otakushelter/frontend \
-otakushelter_port=4004 \
-otakushelter_build_number=$TRAVIS_BUILD_NUMBER\" \
--i inventories/staging"
