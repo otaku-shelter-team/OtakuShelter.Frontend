03a16d2c73c79dc14a819206530531ca1c793421
NoiRTvT <Might_control437>
2019-05-18 07:57:35 +0300

    fix missing in type

--- webclient/src/pages/User/Profile/index.tsx
+++ webclient/src/pages/User/Profile/index.tsx
@@ -4,7 +4,7 @@ import {RouteComponentProps} from 'react-router'
 import TokenService from '../../../services/TokenService'
 
 interface IProfileProps extends RouteComponentProps {
-    lol: number
+    lol?: number
 }
 
 class Profile extends Component<IProfileProps, {}> {
